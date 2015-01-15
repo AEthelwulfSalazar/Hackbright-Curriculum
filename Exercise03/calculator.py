5e84cd592a8e2f96e17aef5065a76edbf318a5b5
Cynthia D <cynthia+cdueltgen@hackbrightacademy.com>
2015-01-15 10:14:51 -0800

    Update calculator.py

--- Exercise03/calculator.py
+++ Exercise03/calculator.py
@@ -4,7 +4,7 @@ calculator.py
 Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
-
+from arithmetic import *
 
 def main():
 	# Your code goes here
