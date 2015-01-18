1a7efabfab470ea2a2a690d9b0992a7757b23b44
Joel Burton <joel@joelburton.com>
2015-01-18 15:13:02 -0800

    Add doctest version.

--- skills1/skills1.py
+++ skills1/skills1.py
@@ -1,48 +1,235 @@
-# Things you should be able to do.
+#!/usr/bin/env python
 
-number_list = [-5, 6, 4, 8, 15, 16, 23, 42, 2, 7]
-word_list = [ "What", "about", "the", "Spam", "sausage", "spam", "spam", "bacon", "spam", "tomato", "and", "spam"]
 
-# Write a function that takes a list of numbers and returns a new list with only the odd numbers.
+"""Hackbright Skills 1: Python Lists.
+
+There are a bunch of functions in this file that are not written, but
+have documentation strings that explain how they should work. Your job
+is to edit this file to write the bodies of these functions.
+We expect that you will solve these problems using Python lists.
+
+This file uses "doctests"; the explanation of how the functions should
+work contains examples just like you'd see in the Python interpreter.
+The examples are actually run and tested when this file is ran.
+When you first run this test, it will show test failures for every
+function--since the real functions haven't been written yet. As you
+write the real functions, your test failure count will go down.
+At the point where you've completed this skills assessment, the
+only output from this program would be:
+
+    ** ALL TESTS PASSED. GOOD WORK!
+
+Good luck!
+"""
+
+
 def all_odd(number_list):
-    return []
+    """Return a list of only the odd numbers in the input list.
 
-# Write a function that takes a list of numbers and returns a new list with only the even numbers.
+        >>> all_odd([1, 2, 7, -5])
+        [1, 7, -5]
+
+        >>> all_odd([2, -6, 8])
+        []
+
+    """
+
+    return [d for d in number_list if d % 2 != 0]
+
+
 def all_even(number_list):
-    return []
+    """Return a list of only the even numbers in the input list.
 
-# Write a function that takes a list of strings and a new list with all strings of length 4 or greater.
+        >>> all_even([2, 6, -1, -2])
+        [2, 6, -2]
+
+        >>> all_even([-1, 3, 5])
+        []
+
+    """
+
+    return [d for d in number_list if d % 2 == 0]
+
+
 def long_words(word_list):
-    return []
+    """Return all words in input list that are longer than 4 characters.
+    
+        >>> long_words(["hello", "hey", "spam", "spam", "bacon", "bacon"])
+        ['hello', 'bacon', 'bacon']
 
-# Write a function that finds the smallest element in a list of integers and returns it.
-def smallest(number_list):
-    return None
+        >>> long_words(["all", "are", "tiny"])
+        []
 
-# Write a function that finds the largest element in a list of integers and returns it.
-def largest(number_list):
-    return None
+    """
 
-# Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
+    return [w for w in word_list if len(w) > 4]
+
+
+def smallest_int(number_list):
+    """Find the smallest integer in a list of integers and return it.
+
+        >>> smallest_int([-5, 2, -5, 7])
+        -5
+
+    If the input list is empty, return None:
+
+        >>> smallest_int([]) is None
+        True
+
+    """
+
+    smallest = None
+    for item in number_list:
+        if smallest is None or item < smallest:
+            smallest = item
+    return smallest
+
+
+def largest_int(number_list):
+    """Find the largest integer in a list of integers and return it.
+
+        >>> largest_int([-5, 2, -5, 7])
+        7
+
+    If the input list is empty, return None:
+
+        >>> largest_int([]) is None
+        True
+
+    """
+
+    largest = None
+    for item in number_list:
+        if largest is None or item > largest:
+            largest = item
+    return largest
+
+
 def halvesies(number_list):
-    return []
+    """Return list of numbers from input list, each divided by two.
 
-# Write a function that takes a list of words and returns a list of all the lengths of those words.
+        >>> halvesies([2, 6, -2])
+        [1.0, 3.0, -1.0]
+
+    If any of the numbers are, make sure you don't round off the half:
+
+        >>> halvesies([1, 5])
+        [0.5, 2.5]
+    
+    """
+
+    return [ i / 2.0 for i in number_list]
+
+
 def word_lengths(word_list):
-    return []
+    """Return the length of words in the input list.
 
-# Write a function (using iteration) that sums all the numbers in a list.
+        >>> word_lengths(["hello", "hey", "hello", "spam"])
+        [5, 3, 5, 4]
+
+    """
+
+    return [len(w) for w in word_list]
+
+
 def sum_numbers(number_list):
-    return 0
+    """Return the sum of all of the numbers in the list.
 
-# Write a function that multiplies all the numbers in a list together.
+    Python has a built-in function, `sum()`, which already does this -- but for
+    this exercise, you should not use it.
+
+        >>> sum_numbers([1, 2, 3, 10])
+        16
+
+    Any empty list should return the sum of zero:
+
+        >>> sum_numbers([])
+        0
+
+    """
+
+    total = 0
+    for i in number_list:
+        total = total + i
+
+    return total
+
+
 def mult_numbers(number_list):
-    return 0
+    """Return product (result of multiplication) of the numbers in the list.
 
-# Write a function that joins all the strings in a list together (without using the join method) and returns a single string.
+        >>> mult_numbers([1, 2, 3])
+        6
+
+    Obviously, if there is a zero in the input, the product will be zero:
+
+        >>> mult_numbers([10, 20, 0, 50])
+        0
+
+    As explained at http://en.wikipedia.org/wiki/Empty_product, if the list is
+    empty, the product should be 1:
+
+        >>> mult_numbers([])
+        1
+    
+    """
+
+    product = 1
+    for i in number_list:
+        product = product * i
+    return product
+
+
 def join_strings(word_list):
-    return ""
+    """Return a string of all input strings joined together.
 
-# Write a function that takes a list of integers and returns the average (without using the avg method)
+    Python ha a built-in method on lists, `join` -- but this exercise, you
+    should not use it.
+
+        >>> join_strings(["spam", "spam", "bacon", "balloonicorn"])
+        'spamspambaconballoonicorn'
+
+    For an empty list, you should return an empty string:
+
+        >>> join_strings([])
+        ''
+
+    """
+
+    result = ""
+    for s in word_list:
+        result = result + s
+    return result
+
+
 def average(number_list):
-    return 0
+    """Return the average (mean) of the list of numbers given.
+
+        >>> average([2, 12, 3])
+        5.666666666666667
+
+    There is no defined answer if the list given is empty. It's fine if
+    this raises an error when given an empty list.
+    """
+
+    total = sum_numbers(number_list)
+    return total / float(len(number_list))
+
+
+
+# Intermediate: join_strings with an optional comma-sep
+#   write these as list comprehension
+# Advanced: find unique long words
+
+
+##############################################################################
+# END OF SKILLS TEST; YOU CAN STOP HERE.
+
+
+if __name__ == "__main__":
+     print
+     import doctest
+     if doctest.testmod().failed == 0:
+         print "** ALL TESTS PASSED. GOOD WORK!" 
+     print
+    
